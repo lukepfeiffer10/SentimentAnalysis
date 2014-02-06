6dcb9fb5f28180df7d73d46ffaa8e8683ae0dffc
lukepfeiffer <lpfeiff10@gmail.com>
2014-02-05 18:37:11 -0600

    Main UI files
    Added the bare minimum files for starting/creating the UI

---
+++ js/main.js
@@ -0,0 +1,44 @@
+﻿/// <reference path="js/backbone.js" />
+/// <reference path="js/underscore.js" />
+
+$(function () {
+    var Sentence = Backbone.Model.extend({
+
+    });
+
+    var SentenceList = Backbone.Collection.extend({
+        model: Sentence
+    });
+
+    var SentenceView = Backbone.View.extend({
+        tagName: 'span',
+        className: 'sentence',
+
+        events: {
+            'click': 'editSentiment'
+        },
+
+        template: _.template("<%= text %>"),
+
+        initialize: function () {
+
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.attributes));
+            return this;
+        },
+
+        editSentiment: function () {
+            alert('You clicked me!');
+        }
+    });
+
+    var Sentences = new SentenceList([new Sentence({id: 1, text: "this is a test!", order: 1}),
+                                new Sentence({id: 2, text: "this is a test 2.", order: 2}),
+                                new Sentence({ id: 3, text: "this is a test 3?", order: 3 })]);
+
+    Sentences.each(function (sentence) {
+        $('#story').append(new SentenceView(sentence).render().$el);
+    });
+});
