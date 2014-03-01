89028a6d42ab49f8be56da86fa7bc3459eda74cc
lukepfeiffer <lpfeiff10@gmail.com>
2014-03-01 14:48:20 -0600

    Loading sentences from database to ui
    Updated the javascript to load sentences from the database and display
    them to the page. Re-arranged some files to allow testing of the static
    html pages

---
+++ api/static/js/main.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="js/backbone.js" />
+/// <reference path="js/underscore.js" />
+
+$(function () {
+    var Sentence = Backbone.Model.extend({
+    });
+
+    var SentenceList = Backbone.Collection.extend({
+		url: '/api/sentences/next',
+        model: Sentence,
+		
+		parse: function(response) {
+			return response.sentences;
+		}
+    });
+
+    var SentenceView = Backbone.View.extend({
+        tagName: 'span',
+        className: 'sentence',
+
+        events: {
+            'click': 'editSentiment'
+        },
+
+        template: _.template("<%= sentence_txt %>"),
+
+        initialize: function () {
+
+        },
+
+        render: function () {
+            this.$el.html(this.template(this.model.attributes));
+            return this;
+        },
+
+        editSentiment: function () {
+            alert('You clicked me!');
+        }
+    });
+
+    /*var Sentences = new SentenceList([new Sentence({id: 1, text: "this is a test!", order: 1}),
+                                new Sentence({id: 2, text: "this is a test 2.", order: 2}),
+                                new Sentence({ id: 3, text: "this is a test 3?", order: 3 })]);*/
+								
+	var Sentences  = new SentenceList();
+	Sentences.fetch({
+		parse: true,
+		success: function(collection, response, options) {
+			collection.each(function(sentence) {
+				$('#story').append(new SentenceView({model: sentence}).render().$el);
+			});
+		}
+	});
+});
--- api/webapi.py
+++ api/webapi.py
@@ -1,15 +1,18 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify, abort, request, make_response, url_for, session
+from flask import Flask, jsonify, abort, request, make_response, url_for, session, render_template
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from sentenceAPI import SentencesAPI
 from userAPI import UserLoginAPI
  
-app = Flask(__name__, static_url_path = "")
+app = Flask(__name__, static_url_path = "/static", template_folder = "../")
 api = Api(app)
 
 api.add_resource(SentencesAPI, '/api/sentences/next', endpoint= 'sentences')
 api.add_resource(UserLoginAPI, '/api/user/login', endpoint= 'login')
+@app.route('/')
+def renderIndex():
+    return render_template('index.html')
 
 app.secret_key = '\xd9\xa4\xf9hQ\x82`k9Y\xca7,0\x05gmj\xee\x16\x98Y\x8b\x98'
 
