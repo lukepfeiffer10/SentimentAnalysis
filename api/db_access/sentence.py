bead0075e23fb0e05e124bec3ddea7ba533bc3e3
Caleb <czimmerman@unomaha.edu>
2014-03-24 16:46:57 -0500

    changes to sentence insert function
    sentence.insert now returns the id of inserted sentence

--- api/db_access/sentence.py
+++ api/db_access/sentence.py
@@ -5,13 +5,17 @@ from db import exec_proc
 # Start sequence at 0 to be consistent
 def insert(story_id, sentence_txt, seq_num):
     rc, rs = exec_proc("usp_ins_sentence", story_id, sentence_txt, seq_num)
-    return True if rc else False
+    return rs[0]['sentence_id'] if rc else False
 
 def select_next(user_id, num_before = 0, num_after = 0):
     rc, rs = exec_proc("usp_sel_sentence", user_id, num_before, num_after)
     return {'sentences': rs} if rc else False
+    
+def select_current_group(user_id):
+    rc, rs = exec_proc("usp_sel_sentence_group", user_id)
+    return {'sentences': rs} if rc else False
 
-# Call this only if isLegit() returns true
+
 def tag(user_id, sentence_id, tag_id):
     rc, rs = exec_proc("usp_tag_sentence", user_id, sentence_id, tag_id)
     return rs if rc else False
@@ -20,3 +24,6 @@ def tag(user_id, sentence_id, tag_id):
 def validate(user_id, sentence_id, tag_id):
     rc, rs = exec_proc("usp_sentence_validate", user_id, sentence_id, tag_id)
     return rs[0]['legit']
+    
+def _increment_tag_counter(user_id):
+    rc, rs = exec_proc("usp_sentence_validate", user_id, sentence_id, tag_id)
