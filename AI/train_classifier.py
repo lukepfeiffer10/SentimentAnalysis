8ae79cb03ff69a78408c46a02a6d2c91b2b7d700
Foshkey <foshkey@gmail.com>
2014-03-26 13:27:38 -0500

    Implemented Custom Corpus

--- AI/train_classifier.py
+++ AI/train_classifier.py
@@ -2,17 +2,21 @@
 
 import pickle
 from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import CategorizedPlaintextCorpusReader
 from featx import *
 
-# to be replaced with our own corpus
-from nltk.corpus import movie_reviews
-corpus = movie_reviews
+# movie_reviews corpus
+# from nltk.corpus import movie_reviews
+# corpus = movie_reviews
+
+# our corpus
+corpus = CategorizedPlaintextCorpusReader('./corpus/', r'.*\.txt', cat_pattern=r'(\w+)\.txt')
 
 # load classifier
 # nb_classifier = pickle.load(open('nb_classifier', 'rb'))
 
 lfeats = label_feats_from_corpus(corpus)
-train_feats, test_feats = split_label_feats(lfeats)
+train_feats, test_feats = split_label_feats(lfeats, 1)
 nb_classifier = NaiveBayesClassifier.train(train_feats)
 
 # dump to file (WARNING: File will be overwritten)
