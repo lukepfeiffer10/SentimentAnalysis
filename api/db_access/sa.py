248900d35c87a4b933aaa8056900c62932c85805
Caleb <czimmerman@unomaha.edu>
2014-03-29 14:33:34 -0500

    added select stem words function
    added select stem words function

--- api/db_access/sa.py
+++ api/db_access/sa.py
@@ -5,3 +5,7 @@ from db import exec_proc
 def insert_liwc_stem(stem):
     rc, rs = exec_proc("usp_ins_liwc_stem", stem)
     return rc
+    
+def select_liwc_stems():
+    rc, rs = exec_proc("usp_sel_liwc_stems")
+    return rs if rc else False
