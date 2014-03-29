74bc4b83e672fdea84aecac23989b541aec6bc59
Caleb <czimmerman@unomaha.edu>
2014-03-29 14:48:50 -0500

    fixed bug

--- api/db_access/sa.py
+++ api/db_access/sa.py
@@ -1,9 +1,8 @@
 from db import exec_proc
 
 
-
-def insert_liwc_stem(stem):
-    rc, rs = exec_proc("usp_ins_liwc_stem", stem)
+def insert_liwc_stem(stem, tag_id):
+    rc, rs = exec_proc("usp_ins_liwc_stem", stem, tag_id)
     return rc
     
 def select_liwc_stems():
