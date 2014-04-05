5e4b84c580489cc754139f982acf89370940a717
Caleb <czimmerman@unomaha.edu>
2014-04-05 16:48:41 -0500

    Added insert keyword function
    Added insert keyword function

--- api/db_access/sa.py
+++ api/db_access/sa.py
@@ -8,3 +8,7 @@ def insert_liwc_stem(stem, tag_id):
 def select_liwc_stems():
     rc, rs = exec_proc("usp_sel_liwc_stems")
     return rs if rc else False
+    
+def insert_keyword(keyword, sentence_id, negation_flg, liwc_tag_id = None):
+    rc, rs = exec_proc("usp_ins_keyword", keyword, sentence_id, liwc_tag_id)
+    return True if rc else False
