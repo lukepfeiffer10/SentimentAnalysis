d01c8a3d1c59c9cdc8cab6d512cbed1882fd7347
lukepfeiffer <lpfeiff10@gmail.com>
2014-04-05 15:53:28 -0500

    Login fix and create user form
    Fixed a bug with the new login request
    
    Added a create new user form to the login page

--- api/static/js/login.js
+++ api/static/js/login.js
@@ -1,13 +1,14 @@
 ﻿/// <reference path="js/jquery.js" />
 
 $(function () {
-    $('#loginButton').click(function () {
+    $('#loginArea').submit(function (ev) {
+        ev.preventDefault();
         var loginObject = {};
         loginObject.username = $('#username').val();
         loginObject.password = $('#password').val();
         $.ajax({
             url: '/api/user', 
-            data: loginObject, 
+            data: JSON.stringify(loginObject), 
             success: function () {
                 window.location = '/';
             },
@@ -15,5 +16,15 @@ $(function () {
             type: 'PUT',
             //contentType: 'application/json'
         });
+    });
+
+    $('#newUserForm').submit(function (ev) {
+        ev.preventDefault();
+        var newUser = {};
+        newUser.firstName = $('#firstName').val();
+        newUser.lastName = $('#lastName').val();
+        newUser.username = $('#newUsername').val();
+        newUser.password = $('#newPassword').val();
+        alert(JSON.stringify(newUser));
     });
 });
