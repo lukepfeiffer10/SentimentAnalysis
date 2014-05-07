4bd31db23e615b34588e631b14fc9fadd6d088ea
Caleb <czimmerman@unomaha.edu>
2014-05-07 17:23:19 -0500

    changes to story cluster
    lsdjf

--- api/clusterAPI.py
+++ api/clusterAPI.py
@@ -12,5 +12,6 @@ class ClusterAPI(Resource):
         super(ClusterAPI,self).__init__()
 
     def get(self):
-        return [{'attr1':1, 'attr2':2, 'sentences': [{ 'sentence_txt':'This is a test sentence for testing purposes. Woo!','attr2':2}, { 'sentence_txt':'This is a another test sentence for testing purposes. Woo!','attr2':2} ]}]
+        return story.select_cluster(132)
+        #return [{'attr1':1, 'attr2':2, 'sentences': [{ 'sentence_txt':'This is a test sentence for testing purposes. Woo!','attr2':2}, { 'sentence_txt':'This is a another test sentence for testing purposes. Woo!','attr2':2} ]}]
         #return [{'attr1':1, 'attr2':2}]
--- api/db_access/story.py
+++ api/db_access/story.py
@@ -1,4 +1,4 @@
-from db import *
+from db import exec_proc, _pretty_print
 
 def insert(user_id, story_title, story_content):
     rc, rs = exec_proc("usp_ins_story", user_id, story_title, story_content)
@@ -12,6 +12,37 @@ def insert_complete(story_id):
 def select_all():
     rc, rs = exec_proc("usp_sel_story_all")
     return {'stories': rs} if rc else False
+
+def select_cluster(story_id):
+    # This needs to be changed
+    rc, rs = exec_proc("usp_sel_cluster", story_id)
+    curr_cluster_id = 0
+    arr = []
+    sentences = []
+    i = 0
+    for v in rs:
+        if i == 0:
+            dict = {'sentiment': v['sentiment'].encode("utf-8"), 'length': v['length'], 'weight': v['weight'], 'type': v['type'].encode('utf-8')}
+            sentences.append({'sentence_txt': v['sentence_txt']})
+            curr_cluster_id = v['cluster_id']
+        else:
+            if v['cluster_id'] == curr_cluster_id:
+                sentences.append({'sentence_txt': v['sentence_txt']})
+            else:
+                # finish dict and append to arr and reset
+                dict['sentences'] = sentences
+                arr.append(dict)
+                dict = {'sentiment': v['sentiment'].encode("utf-8"), 'length': v['length'], 'weight': v['weight'], 'type': v['type'].encode('utf-8')}
+                sentences = [{'sentence_txt': v['sentence_txt']}]
+                curr_cluster_id = v['cluster_id']
+    # last group
+    
+        i = 1
+    return arr
+    #print arr
+            
+            
+    #return rs if rc else False
 
 def select_all_by_author(user_id):
     rc, rs = exec_proc("usp_sel_story_all_by_author", user_id)
